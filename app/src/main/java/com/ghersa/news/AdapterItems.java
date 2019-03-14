f03e92222366cdb06f8c38e049eab37d0d2a229c
ghersa <ghersa.sofiane.m2@gmail.com>
2019-03-14 22:44:18 +0100

    ADD AdapterItems

---
+++ app/src/main/java/com/ghersa/news/AdapterItems.java
@@ -0,0 +1,59 @@
+package com.ghersa.news;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AdapterItems extends ArrayAdapter<String> {
+    private final int layout;
+    ImageButton imageButton;
+
+    public AdapterItems(@NonNull Context context, int resource, @NonNull List<String> objects) {
+        super(context, resource, objects);
+        layout = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view;
+        if (convertView == null){
+            // Create new view
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            view = inflater.inflate(layout, null);
+        } else {
+            view = convertView;
+        }
+
+        String item = getItem(position);
+
+        // Recycle view
+        TextView titleView = view.findViewById(R.id.Titre);
+
+        imageButton = view.findViewById(R.id.Favori);
+        titleView.setText(item);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+ 
+                Log.i("GHERSA","SALUTTTTT " + position + " :: " + getItem(position));
+            }
+        });
+
+        return view;
+
+    }
+
+
+}
--- app/src/main/java/com/ghersa/news/MainActivity.java
+++ app/src/main/java/com/ghersa/news/MainActivity.java
@@ -2,12 +2,68 @@ package com.ghersa.news;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    final List<String> items = new ArrayList<>();
+    int cont = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item);
+        setContentView(R.layout.activity_main);
+
+
+
+
+            ListView lv = findViewById(R.id.listView);
+
+            items.add("Item 1");
+            items.add("Item 2");
+            items.add("Item 3");
+            items.add("Item 4");
+            items.add("Item 5");
+            items.add("Item 6");
+            items.add("Item 7");
+            items.add("Item 8");
+            items.add("Item 9");
+            items.add("Item 10");
+            items.add("Item 11");
+            items.add("Item 12");
+            items.add("Item 13");
+            items.add("Item 14");
+            items.add("Item 7");
+            items.add("Item 8");
+            items.add("Item 9");
+            items.add("Item 10");
+            items.add("Item 11");
+            items.add("Item 12");
+            items.add("Item 13");
+            items.add("Item 14");
+
+            final ArrayAdapter<String> adapter = new
+
+                    AdapterItems(
+                    this,
+                    R.layout.item,
+                    items);
+
+            lv.setAdapter(adapter);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Log.i("GHERSA","SALUTTTTT :: "+ position);
+                }
+            });
+
+
     }
 }
